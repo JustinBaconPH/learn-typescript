d2ad2185cc128dfea8d4662999844237cefd204c
JustinBaconUST <justinbaconph@gmail.com>
2024-07-04 13:29:29 -0400

    Functions

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -1,2 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Justin";
 console.log(greetings);
+// number
+var userId = 334455.3;
+userId.toFixed();
+//userId = "Justin";
+// boolean
+var isLoggedIn = false;
--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -1,9 +1,21 @@
 let greetings: string = "Hello Justin";
-
-let mynum = 6;
-
-greetings.toUpperCase();
-
 console.log(greetings);
+
+// number
+let userId = 334455.3
+userId.toFixed()
+//userId = "Justin";
+
+// boolean
+let isLoggedIn: boolean = false
+
+//any
+let hero;
+
+function getHero(){
+    return "Thor"
+}
+
+hero = getHero()
 
 export {}
