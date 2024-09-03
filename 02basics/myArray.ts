befeec332637dca37732a594b7ee5ab73353a0e2
JustinBaconUST <justinbaconph@gmail.com>
2024-09-03 17:16:42 -0400

    Arrays

---
+++ 02basics/myArray.ts
@@ -0,0 +1,20 @@
+const students: string[] = []
+// const studentClass: number[] = []
+const studentClass: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255,255,255],
+    []
+]
+
+students.push("MisonoMika")
+studentClass.push(2)
+
+allUsers.push({name: "", isActive: true})
